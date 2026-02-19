5031458ac3d4ad7cd341f57a4f25da10725d8b3a
Levi McCallum <levi@levimccallum.com>
2026-02-19 10:36:02 -0800

    chore: remove legacy flow_entered system event constant

--- nuxie-core/src/main/kotlin/io/nuxie/sdk/events/SystemEventNames.kt
+++ nuxie-core/src/main/kotlin/io/nuxie/sdk/events/SystemEventNames.kt
@@ -8,7 +8,6 @@ package io.nuxie.sdk.events
 object SystemEventNames {
   const val screenShown: String = "\$screen_shown"
   const val screenDismissed: String = "\$screen_dismissed"
-  const val flowEntered: String = "\$flow_entered"
 
   const val purchaseCompleted: String = "\$purchase_completed"
   const val purchaseFailed: String = "\$purchase_failed"
@@ -18,4 +17,3 @@ object SystemEventNames {
   const val restoreFailed: String = "\$restore_failed"
   const val restoreNoPurchases: String = "\$restore_no_purchases"
 }
-
